8511e5eb12385720422c2caaeac18a8108a9f032
Freya-29 <freya19beceg147@gmail.com>
2022-10-18 17:46:32 +0530

    app2.2

--- src/app/payment/paynow/paynow.component.ts
+++ src/app/payment/paynow/paynow.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer } from 'src/app/customer/customer.model';
+import { DataservicesService } from 'src/app/service/dataservices.service';
+import { Router,ActivatedRoute, RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-paynow',
   templateUrl: './paynow.component.html',
-  styleUrls: ['./paynow.component.css']
+  styleUrls: ['./paynow.component.css'],
+  providers: [DatePipe]
 })
+
+
 export class PaynowComponent implements OnInit {
 
-  constructor() { }
+  id:any;
+  arr!:Customer;
+  allData: Customer[];
+  localItem!: string | null;
+  myDate:any = new Date();
+
+
+ 
+
+  constructor(private route:ActivatedRoute,private _getdata:DataservicesService,private router: Router,private datePipe: DatePipe){
+
+    this.myDate = this.datePipe.transform(this.myDate, 'yyyy-MM-dd');
+
+    this.id=this.route.snapshot.params['id'];
+    this.allData = this._getdata.getData();
+    const filterd = this.allData.filter(ele => ele.id === this.id);
+    this.arr = filterd[0];
+    console.log(this.arr);
+  }
+
+  getCartDetails:any=[];
+  CartDetails(){
+    if(localStorage.getItem('localCart')){
+      this.getCartDetails = JSON.parse(localStorage.getItem('localCart') || "[]");
+      console.log(this.CartDetails);      
+    }
+  }
 
   ngOnInit(): void {
+    this.CartDetails();
   }
 
 }
